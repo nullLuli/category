43a348a1dd2ee151b647a727957c0d67d1719ffb
nullLuli <2323598470@qq.com>
2017-06-08 18:24:57 +0800

    初始化工程

---
+++ Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Category
+//
+//  Created by 李璐 on 2017/6/8.
+//  Copyright © 2017年 李璐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
